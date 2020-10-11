7ff2d88e12acac01cb426b3b59d9dd4918167c89
Wilmx <wilberthdelao@gmail.com>
2020-10-11 16:21:20 -0500

    Se agregó función para M3 a barriles y viceversa

--- conversor_unidades_medida.py
+++ conversor_unidades_medida.py
@@ -1,38 +1,38 @@
-def psi_kilos__cm2(psi):
+def psi_kilos__cm2(psi):  # Psi a kilos
     kilos_cm2 = psi / 14.22
     return kilos_cm2
 
 
-def kilos_cm2__psi(kilos_cm2):
+def kilos_cm2__psi(kilos_cm2):  # Kilos a Psi
     psi = kilos_cm2 * 14.22
     return psi
 
 
-def barriles__litros(barriles):
+def barriles__litros(barriles):  # Barriles a litros
     litros = barriles * 158.99
     return litros
 
 
-def litros__barriles(litros):
+def litros__barriles(litros):  # Litros a barriles
     barriles = litros / 158.99
     return barriles
 
 
-def litros__m3(litros):
+def litros__m3(litros):  # Litros a M3
     m3 = litros / 1000
     return m3
 
 
-def m3__litros(m3):
+def m3__litros(m3):  # M3 a litros
     litros = m3 * 1000
     return litros
 
 
-def m3__barriles(m3):
+def m3__barriles(m3):  # M3 a barriles
     barriles = m3 / 6.29
     return barriles
 
 
-def barriles__m3(barriles):
+def barriles__m3(barriles): # Barriles a M3
     m3 = barriles * 6.29
     return m3
